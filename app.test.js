05f7be29072da2b2a2f46cfd3a3ea1f1241872f4
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-10-23 09:32:45 -0500

    tercer commit con los tests arreglados y workflow en OK

--- app.test.js
+++ app.test.js
@@ -1,17 +1,18 @@
 // app.test.js
+
 const { getMessiInfo, getTituloPrincipal } = require('./app');
 
-// Test unitario con error intencional (nombre incorrecto)
+// Test unitario corregido (nombre correcto)
 test('Debe devolver información correcta de Messi', () => {
 const info = getMessiInfo();
-expect(info.nombre).toBe("Lionel Andres Messi"); // Error intencional
+expect(info.nombre).toBe("Lionel Messi"); // corregido
 expect(info.seleccion).toBe("Argentina");
 });
 
-// Test de integración con error intencional (cantidad incorrecta)
+// Test de integración corregido (cantidad incorrecta)
 test('Debe generar una frase con el número correcto de títulos', () => {
 const info = getMessiInfo();
 const frase = getTituloPrincipal(info);
-expect(info.titulos.length).toBe(6); // Error intencional
+expect(info.titulos.length).toBe(4); // corregido
 expect(frase).toContain("Lionel Messi ha ganado");
 });
