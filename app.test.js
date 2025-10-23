b344f7e0df31446fce45796c7ddce37ef6e5a7d5
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-10-23 09:28:31 -0500

    segundo commit con los tests fallidos intencionalmente

--- app.test.js
+++ app.test.js
@@ -1,18 +1,17 @@
 // app.test.js
-
 const { getMessiInfo, getTituloPrincipal } = require('./app');
 
-// Test unitario corregido (nombre correcto)
+// Test unitario con error intencional (nombre incorrecto)
 test('Debe devolver información correcta de Messi', () => {
 const info = getMessiInfo();
-expect(info.nombre).toBe("Lionel Messi"); // corregido
+expect(info.nombre).toBe("Lionel Andres Messi"); // Error intencional
 expect(info.seleccion).toBe("Argentina");
 });
 
-// Test de integración corregido (cantidad incorrecta)
+// Test de integración con error intencional (cantidad incorrecta)
 test('Debe generar una frase con el número correcto de títulos', () => {
 const info = getMessiInfo();
 const frase = getTituloPrincipal(info);
-expect(info.titulos.length).toBe(4); // corregido
+expect(info.titulos.length).toBe(6); // Error intencional
 expect(frase).toContain("Lionel Messi ha ganado");
 });
